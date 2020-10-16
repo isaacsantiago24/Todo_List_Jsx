43675d1a69089720f9b9f5552e31a864cda4e526
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2020-10-15 21:59:39 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+//importing components
+import Form from './components/Form';
+import TodoList from "./components/TodoList"
+
 function App() {
+  //inputText = value
+  //setInputText= function that allows you to update inputText
+  const [inputText,setInputText]=useState("");
+  //store to-dos
+  const[todos, setTodos]=useState([]);
+  const[status, setStatus] = useState('all');
+  const[filteredTodos,setFilterTodos]=useState([]);
+
+  //USE EFFECT
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos,status]);
+
+  //function//events
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilterTodos(todos.filter(todo => todo.completed === true));
+        break;
+
+      case 'uncompleted':
+        setFilterTodos(todos.filter(todo => todo.completed === false));
+        break;
+
+      default:
+        setFilterTodos(todos);
+        break;
+    }
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Isaac's Todo List</h1>
       </header>
+
+      <Form 
+      inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      setStatus={setStatus}
+      
+      />
+      
+      <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}/>
+     
     </div>
   );
 }
